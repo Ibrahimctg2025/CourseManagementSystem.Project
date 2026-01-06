7ecdc8fbc870ee8a52c68b5110edb0fb0b55f123
MD.IBRAHIM <ibrahimdk71@gmail.com>
2026-01-06 15:43:52 +0600

    Add files via upload

---
+++ CourseManagementSystem.Core/Enums/EnrollmentStatus.cs
@@ -0,0 +1,10 @@
+namespace CourseManagementSystem.Core.Enums;
+
+public enum EnrollmentStatus
+{
+    Processing = 0,
+    Enrolled = 1,
+    Completed = 2,
+    Withdrawn = 3,
+    Suspended = 4
+}
---
+++ CourseManagementSystem.Core/Enums/PaymentStatus.cs
@@ -0,0 +1,9 @@
+namespace CourseManagementSystem.Core.Enums;
+
+public enum PaymentStatus
+{
+    Pending = 0,
+    Paid = 1,
+    UnPaid = 2,
+    Returned = 3
+}
